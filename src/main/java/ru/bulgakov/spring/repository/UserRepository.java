6b3ff77ddac45117b5d5db2613461122e68bf874
Alexander Bulgakov <dev@asbulgakov.ru>
2025-09-01 20:11:21 +0300

    Add @Repository

--- src/main/java/ru/bulgakov/spring/repository/UserRepository.java
+++ src/main/java/ru/bulgakov/spring/repository/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.bulgakov.spring.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Override
